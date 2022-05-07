fd47c5cf9b77176f9579ed67f9a9692af7012555
danielhillsman <daniel.loves.junk@gmail.com>
2022-05-07 12:47:46 -0700

    added screenshots and README

---
+++ build/precache-manifest.ca11760df8e56856dc09f7b2dbfc6a7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fb66f9c1ed094698456",
+    "url": "/My-Portfolio-React/static/css/main.335c1386.chunk.css"
+  },
+  {
+    "revision": "3fb66f9c1ed094698456",
+    "url": "/My-Portfolio-React/static/js/main.3fb66f9c.chunk.js"
+  },
+  {
+    "revision": "ccbab249f913b0dfaa78",
+    "url": "/My-Portfolio-React/static/css/1.b509c1f3.chunk.css"
+  },
+  {
+    "revision": "ccbab249f913b0dfaa78",
+    "url": "/My-Portfolio-React/static/js/1.ccbab249.chunk.js"
+  },
+  {
+    "revision": "bdbec736e52f3c8f66dd",
+    "url": "/My-Portfolio-React/static/js/runtime~main.bdbec736.js"
+  },
+  {
+    "revision": "ddaf65ff777b071bee5db9cd888ac3a6",
+    "url": "/My-Portfolio-React/index.html"
+  }
+];
